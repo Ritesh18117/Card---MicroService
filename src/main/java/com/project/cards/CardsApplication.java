319bf1337f8803389058654135a1735e0f0f33fd
Main <ritesh.gupta.18117@gmail.com>
2025-09-04 02:34:43 +0530

    Final Commit

---
+++ src/main/java/com/project/cards/CardsApplication.java
@@ -0,0 +1,40 @@
+package com.project.cards;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Cards microservice REST API Documentation",
+                description = "bank Cards microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Ritesh Gupta",
+                        email = "ritesh.gupta.18117@gmail.com",
+                        url = "https://www.linkedin.com/in/ritesh-kumar-gupta-3648ab187/"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.linkedin.com/in/ritesh-kumar-gupta-3648ab187/"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Bank Cards microservice REST API Documentation",
+                url = "https://www.linkedin.com/in/ritesh-kumar-gupta-3648ab187/"
+        )
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+
+}
